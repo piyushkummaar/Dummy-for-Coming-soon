13ed07a4d68878395b7186add927d46bf3920743
Piyush Kumar <46808299+piyushkummaar@users.noreply.github.com>
2024-07-30 13:06:37 +0530

    few changes

--- Project/settings.py
+++ Project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vb-jt_dz3)^81uo-uxw2h5t5@t6uqv%80-+5yswfg(8bi(+i6b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
